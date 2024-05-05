b7484bab151930a9b64fb4aad5d6a2e7c1b83a0f
Vikas Pathak <pathakvikas190@gmail.com>
2024-05-06 01:01:35 +0530

    Added Singleton Design Pattern

---
+++ src/designpattern/Singleton/DBConnection.java
@@ -0,0 +1,32 @@
+package designpattern.Singleton;
+
+public class DBConnection {
+  public static DBConnection instance = null;
+
+  private DBConnection() {}
+
+  public static DBConnection getInstance() {
+
+    if(instance != null) return instance;
+
+    // Thread Safe Locking
+    synchronized (DBConnection.class) {
+
+      // Check if instance is already created then return that instance
+      if(instance == null) {
+        instance = new DBConnection();
+      }
+      return instance;
+
+    }
+  }
+
+  /**
+   * This is the key method which is responsible during deserialization process
+   * This method get invoked, and we are simply returning already created object
+   * @return
+   */
+  protected Object readResolve() {
+    return instance;
+  }
+}
